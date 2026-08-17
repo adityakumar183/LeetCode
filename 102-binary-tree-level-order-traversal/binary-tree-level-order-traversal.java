5eeb8b3e1ad837988851b5c45ffebd34be7672ef
Aditya Kumar <adityakumar26102004@gmail.com>
2026-08-17 16:22:20 +0530

    Time: 37 ms (1.62%) | Memory: 46.8 MB (42.03%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -14,26 +14,48 @@
  * }
  */
 class Solution {
+    public int levels(TreeNode root) {
+        if (root == null) return 0;
+        int level = 1 + Math.max(levels(root.left), levels(root.right));
+        return level;
+    }
+
+    public void nThLevel(TreeNode root, int level,int lvl,List<Integer> arr){
+        if (root == null) return;
+        if(level==lvl) arr.add(root.val);
+        nThLevel(root.left, level+1,lvl,arr);
+        nThLevel(root.right, level+1,lvl,arr);
+    }
+
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (root == null) return result;
+        // List<List<Integer>> result = new ArrayList<>();
+        // if (root == null) return result;
 
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
+        // Queue<TreeNode> q = new LinkedList<>();
+        // q.add(root);
 
-        while (q.size()>0) {
-            int size = q.size();
-            List<Integer> level = new ArrayList<>();
+        // while (q.size()>0) {
+        //     int size = q.size();
+        //     List<Integer> level = new ArrayList<>();
 
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-                level.add(node.val);
+        //     for (int i = 0; i < size; i++) {
+        //         TreeNode node = q.poll();
+        //         level.add(node.val);
 
-                if (node.left != null) q.add(node.left);
-                if (node.right != null) q.add(node.right);
-            }
-            result.add(level);
+        //         if (node.left != null) q.add(node.left);
+        //         if (node.right != null) q.add(node.right);
+        //     }
+        //     result.add(level);
+        // }
+        // return result;
+
+        List<List<Integer>> ans = new ArrayList<>();
+        int lvl = levels(root);
+        for(int i = 0;i<=lvl-1;i++){
+            List<Integer> arr = new ArrayList<>();
+            nThLevel(root,0,i,arr);
+            ans.add(arr);
         }
-        return result;
+        return ans;
     }
 }
