c6d0f2aa8d0536c11866aa873fa6695363488e27
Aditya Kumar <adityakumar26102004@gmail.com>
2026-08-19 18:17:44 +0530

    Time: 1 ms (70.08%) | Memory: 43.9 MB (15.28%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -16,9 +16,9 @@
 class Solution {
     public void dfs(TreeNode root, int level,List<Integer> ans){
         if(root==null) return;
+        ans.set(level,root.val);
         dfs(root.left,level+1,ans);
         dfs(root.right,level+1,ans);
-        ans.set(level,root.val);
     }
 
     public int levels(TreeNode root){
