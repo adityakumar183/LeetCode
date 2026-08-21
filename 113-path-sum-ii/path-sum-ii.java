b3b5c35e2a7a132aa380b6d8413c3765ac7c7e1b
Aditya Kumar <adityakumar26102004@gmail.com>
2026-08-22 00:10:54 +0530

    Time: 12 ms (2.29%) | Memory: 49.2 MB (5.03%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -14,25 +14,34 @@
  * }
  */
 class Solution {
-    public void dfs(TreeNode node, int target, List<Integer> path, List<List<Integer>> result){
-       if(node == null) return;
-
-       path.add(node.val);
-       target -= node.val;
-
-       if (node.left == null && node.right == null && target == 0) {
-            result.add(new ArrayList<>(path));
-        } else {
-            dfs(node.left, target, path, result);
-            dfs(node.right, target, path, result);
+    public List<Integer> copy(List<Integer> arr){
+        List<Integer> list = new ArrayList<>();
+        for(int ele : arr){
+            list.add(ele);
         }
-
-        path.remove(path.size() - 1);
+        return list;
     }
 
-    public List<List<Integer>> pathSum(TreeNode root, int targetsum) {
-        List<List<Integer>> result = new ArrayList<>();
-        dfs(root,targetsum,new ArrayList<>(), result);
-        return result;
+    public void helper(TreeNode root, int target, List<Integer>arr, List<List<Integer>> ans){
+        if(root==null) return;
+        if(root.left==null && root.right==null){
+            if(root.val==target){
+                arr.add(root.val);
+                ans.add(arr);
+            }
+            return;
+        }
+        arr.add(root.val);
+        List<Integer> arr1 = copy(arr);
+        List<Integer> arr2 = copy(arr);
+        helper(root.left,target-root.val,arr1,ans);
+        helper(root.right,target-root.val,arr2,ans);
+    }
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> arr = new ArrayList<>();
+        helper(root,targetSum,arr,ans);
+        return ans;
     }
 }
